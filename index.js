25097ec0f0423e75c3177f584dc9ebbba7e194c4
a.iusupov <kleot@mail>
2022-12-21 18:46:21 +0300

    init setup of express app

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const express = require("express")
 
 const app = express()
 
+app.get('/', (req, res, next) => {
+    res.send('<h2>Hello World</h2>')
+})
+
 const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log('App is running...')
+})
