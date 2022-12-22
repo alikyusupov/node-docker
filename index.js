9c7622f86531e68937c3c89c12e986613498ffd1
a.iusupov <kleot@mail>
2022-12-22 14:44:23 +0300

    add configs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get('/', (req, res, next) => {
-    res.send('<h2>Hello World!!!Bye!</h2>')
+    res.send('<h2>Hello World!Bye!!Temur</h2>')
 })
 
 const port = process.env.PORT || 3000
